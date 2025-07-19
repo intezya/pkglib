181b3ed8eb87e9d42a14e7a600d70167dfe26375
Tim <intezya@gmail.com>
2025-07-19 15:02:05 +0400

    feat: return itertools map function

---
+++ itertools/map.go
@@ -0,0 +1,11 @@
+package itertools
+
+func Map[T, R any](items []T, fn func(T) R) []R {
+	result := make([]R, len(items))
+
+	for i, item := range items {
+		result[i] = fn(item)
+	}
+
+	return result
+}
